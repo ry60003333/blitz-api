c27f19b199621801afdd92c0a4c4277099b0c1b0
Ryan Rule-Hoffman <ry60003333@mac.com>
2013-10-23 12:26:59 -0400

    Initial commit.

---
+++ src/org/allgofree/rs2blitz/api/Plugin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.allgofree.rs2blitz.api;
+
+/**
+ *
+ * @author ry60003333
+ */
+public interface Plugin
+{
+    
+}
